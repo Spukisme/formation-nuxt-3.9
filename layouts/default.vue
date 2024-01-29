4b48cb52ce472561f254aab3b85d473a3d6fb17d
Laurent <laurent.cuadras@gmail.com>
2024-01-29 14:39:44 +0100

    Correction(DarkMode)

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import AppDarkModeBtn from '~/components/AppDarkModeBtn.vue'
+
+  const darkMode = ref<'light' | 'dark'>('dark')
+</script>
+
+<template>
+  <VApp :theme="darkMode">
+    <VLayout class="rounded rounded-md">
+      <VNavigationDrawer>
+        <VList>
+          <VListItem title="Navigation drawer"></VListItem>
+        </VList>
+      </VNavigationDrawer>
+
+      <VAppBar title="Application bar">
+        <AppDarkModeBtn v-model="darkMode" />
+      </VAppBar>
+
+      <VMain
+        class="d-flex align-center justify-center"
+        style="min-height: 300px"
+      >
+        <slot />
+      </VMain>
+    </VLayout>
+  </VApp>
+</template>
+
+<style>
+  html {
+    overflow: auto;
+  }
+</style>
