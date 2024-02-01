c9648f328d5d3fae01c5682b49320ab4dde4614a
Laurent <laurent.cuadras@gmail.com>
2024-02-01 14:00:52 +0100

    Correction(DeleteFetch)

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -2,9 +2,23 @@
   import type {UserInterface} from '~/types/user'
   import {appCrudTableHeadersConst} from '~/constants/appCrudTableHeaders.const'
 
-  const {data, pending, error} = useFetch<Array<UserInterface>>('/api/users', {
-    default: () => [] as Array<UserInterface>,
-  })
+  const {data, pending, error, refresh} = useFetch<Array<UserInterface>>(
+    '/api/users',
+    {
+      default: () => [] as Array<UserInterface>,
+    },
+  )
+
+  const handleDeleteUser = async (user: UserInterface) => {
+    await useFetch(`/api/users/${user.id}`, {
+      method: 'DELETE',
+      onResponse: ({response}) => {
+        if (response.ok) {
+          refresh()
+        }
+      },
+    })
+  }
 </script>
 
 <template>
@@ -18,6 +32,7 @@
     :edit-item-function="
       (user: UserInterface) => useRouter().push(`/users/update/${user.id}`)
     "
+    :delete-item-function="handleDeleteUser"
   />
 </template>
 
